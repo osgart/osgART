b5b995e3e4954fe5cffa48f1272db7bbc1d1e537
prl25 <prl25@c80831f2-abe5-5e40-be4e-a8b7f943aa1a>
2006-09-28 02:51:33 +0000

    Added cleanup routine in osgART/Foundation, called via atexit(). Update version number in Mac project files.
    git-svn-id: svn://hitlfs.canterbury.ac.nz/osgART_NG/trunk@121 c80831f2-abe5-5e40-be4e-a8b7f943aa1a

--- src/osgART/Foundation.cpp
+++ src/osgART/Foundation.cpp
@@ -7,7 +7,12 @@
  *
  */
 
+//
+// Includes.
+//
 #include "osgART/Foundation"
+
+#include <stdlib.h>
 
 #ifdef __APPLE__
 #  include <Carbon/Carbon.h>
@@ -15,13 +20,43 @@
 #  include <sys/param.h>
 #endif
 
+//
+// Private globals.
+//
+#ifdef __APPLE__
+static char g_cwd[MAXPATHLEN] = "";
+#endif __APPLE__
+
+//
+// Private functions.
+//
+extern "C" void osgARTFinal(void)
+{
+	// Add OS-specific cleanup here.
+#ifdef __APPLE__
+	// Restore working directory to what it was when we were exec'ed.
+	if (g_cwd[0]) {
+		chdir(g_cwd);
+		g_cwd[0] = '\0';
+	}
+#endif __APPLE__
+}
+
+//
+// Public functions.
+//
 void osgARTInit(int *argcp, char **argv)
 {
+	// Register exit function before anything else.
+	atexit(osgARTFinal);
+	
+	// Add OS-specific setup here.
 #ifdef __APPLE__
 		// On Mac OS X, ARToolKit Data is in "Resources" directory inside the application bundle.
+		if (!g_cwd[0]) getcwd(g_cwd, MAXPATHLEN);
 		CFURLRef pathCFURLRef = CFBundleCopyResourcesDirectoryURL(CFBundleGetMainBundle()); // Get relative path to resources directory.
 		if (pathCFURLRef) {
-			char *path = (char *)calloc(MAXPATHLEN, sizeof(char)); //getcwd(path, MAXPATHLEN);
+			char *path = (char *)calloc(MAXPATHLEN, sizeof(char));
 			if (CFURLGetFileSystemRepresentation(pathCFURLRef, true, (UInt8*)path, MAXPATHLEN)) { // true in param 2 resolves against base.
 				if (chdir(path) != 0) {
 					fprintf(stderr, "Unable to change working directory to %s.\n", path);
