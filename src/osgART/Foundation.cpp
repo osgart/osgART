043038d05bda4a18c734c456953497964b409fc5
prl25 <prl25@c80831f2-abe5-5e40-be4e-a8b7f943aa1a>
2006-09-29 01:39:20 +0000

    Added workaround (in osgARTInit()) for osgText-tickled Mac RadeonX1600 driver bug.
    git-svn-id: svn://hitlfs.canterbury.ac.nz/osgART_NG/trunk@125 c80831f2-abe5-5e40-be4e-a8b7f943aa1a

--- src/osgART/Foundation.cpp
+++ src/osgART/Foundation.cpp
@@ -18,6 +18,8 @@
 #  include <Carbon/Carbon.h>
 #  include <unistd.h>
 #  include <sys/param.h>
+#  include <OpenGL/OpenGL.h>
+#  include <osgText/Version>
 #endif
 
 //
@@ -51,22 +53,51 @@ void osgARTInit(int *argcp, char **argv)
 	atexit(osgARTFinal);
 	
 	// Add OS-specific setup here.
-#ifdef __APPLE__
-		// On Mac OS X, ARToolKit Data is in "Resources" directory inside the application bundle.
-		if (!g_cwd[0]) getcwd(g_cwd, MAXPATHLEN);
-		CFURLRef pathCFURLRef = CFBundleCopyResourcesDirectoryURL(CFBundleGetMainBundle()); // Get relative path to resources directory.
-		if (pathCFURLRef) {
-			char *path = (char *)calloc(MAXPATHLEN, sizeof(char));
-			if (CFURLGetFileSystemRepresentation(pathCFURLRef, true, (UInt8*)path, MAXPATHLEN)) { // true in param 2 resolves against base.
-				if (chdir(path) != 0) {
-					fprintf(stderr, "Unable to change working directory to %s.\n", path);
-					perror(NULL);
-				} // else fprintf(stderr,"Current working directory is %s\n", path);
-			}
-			free(path);
-			CFRelease(pathCFURLRef);
-		}
-#endif
 
+#ifdef __APPLE__
+	// On Mac OS X, ARToolKit 'Data' is in "Resources" directory inside the application bundle.
+	if (!g_cwd[0]) getcwd(g_cwd, MAXPATHLEN);
+	CFURLRef pathCFURLRef = CFBundleCopyResourcesDirectoryURL(CFBundleGetMainBundle()); // Get relative path to resources directory.
+	if (pathCFURLRef) {
+		char *path = (char *)calloc(MAXPATHLEN, sizeof(char));
+		if (CFURLGetFileSystemRepresentation(pathCFURLRef, true, (UInt8*)path, MAXPATHLEN)) { // true in param 2 resolves against base.
+			if (chdir(path) != 0) {
+				fprintf(stderr, "Unable to change working directory to %s.\n", path);
+				perror(NULL);
+			} // else fprintf(stderr,"Current working directory is %s\n", path);
+		}
+		free(path);
+		CFRelease(pathCFURLRef);
+	}
+	
+	// Workaround for the Radeon X1600 driver bug, which gets tickled by osgText.
+	// See if the ATi Radeon X1600 OpenGL engine is loaded.
+	CGLRendererInfoObj rend;
+	long rendIndex, rendCount, rendProp;
+	if (CGLQueryRendererInfo(0xffffffff, &rend, &rendCount) == kCGLNoError) {
+		for (rendIndex = 0; rendIndex < rendCount; rendIndex++) {
+			if (CGLDescribeRenderer(rend, rendIndex, kCGLRPRendererID, &rendProp) == kCGLNoError) {
+				if (rendProp == 0x00021901) break; // kCGLRendererATIRadeonX1600ID = 0x00021901.
+			}
+		}
+		CGLDestroyRendererInfo(rend);
+		if (rendIndex != rendCount) {
+			// If it is loaded, check if this is this is the Mac OS X version with a dodgy version of this renderer.
+			SInt32 MacVersion;
+			if (Gestalt(gestaltSystemVersion, &MacVersion) == noErr) {
+				if (MacVersion == 0x1047) { // Mac OS X 10.4.7.
+					printf("WARNING: Found a dodgy OpenGL driver for this CPU and Mac OS X version.\n");
+					if (strncmp(osgTextGetVersion(), "1.1", 3) == 0) {
+						printf("You MUST upgrade to OpenSceneGraph version 1.2 to avoid a crash in osgText!!\n");
+						sleep(10);
+					} else {
+						printf("Using workaround in osgText.\n");
+						putenv("OSG_TEXT_INCREMENTAL_SUBLOADING=OFF");
+					}
+				}
+			}
+		}
+	}	
+#endif	
 }
 
