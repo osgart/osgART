12f637ebcceac8e1f42b964d82c60e89a0c322bc
jho76 <jho76@c80831f2-abe5-5e40-be4e-a8b7f943aa1a>
2006-12-21 19:27:44 +0000

    Artoolkit tracker + automatic multi-marker tracking, see marker configuration file multi-marker_auto.dat for an example configuration and notice the new marker type 'AUTO'
    basic idea: 
    Measure the positions of AUTO markers online;
    
    there are 
    - 'trusted markers' whose position is known
    - 'candidate markers' whose position needs to be estimated
    
    In the beginning there is only one trusted marker (i.e. the first one listed in the configuration file)
    this marker also defines the coordinate system's origin.
    When having a trusted marker and a candidate marker in the fov of the camera the candidate's position is measured and averaged.
    After (default 50) measurements the candidate marker becomes a trusted marker (see console output).
    
    This tracker works but in it's current version it's not very accurate. More effort needs to be put into a better position estimation than simply averaging over the first N measurements (e.g. look at median, variance, make continuous measurements..)
    
    Please contact me if you encounter bugs or errors or want to make any compliments :-)
    
    Jens
    
    git-svn-id: svn://hitlfs.canterbury.ac.nz/osgART_NG/trunk@325 c80831f2-abe5-5e40-be4e-a8b7f943aa1a

---
+++ src/osgART/Tracker/ARToolKitMMAuto/AutoMarker.cpp
@@ -0,0 +1,156 @@
+#include <Producer/RenderSurface>
+#include <osgProducer/Viewer>
+
+#include <osg/Node>
+#include <osg/Group>
+#include <osg/Geode>
+#include <osg/Projection>
+#include <osg/AutoTransform>
+#include <osg/ShapeDrawable>
+#include <osg/Geometry>
+#include <osgText/Font>
+#include <osgText/Text>
+#include <osg/Matrix>
+
+#include <osgART/Foundation>
+#include <osgART/VideoManager>
+#include <osgART/ARTTransform>
+#include <osgART/TrackerManager>
+#include <osgART/VideoBackground>
+#include <osgART/VideoPlane>
+#include <osg/io_utils>
+
+#include "AutoMarker.h"
+
+
+// += operator for osg matrices
+osg::Matrix& operator += (osg::Matrix& m1, const osg::Matrix& m2) 
+{
+	for (register int i = 0; i < 4;i++)
+	for (register int j = 0; j < 4;j++)
+	m1(i,j) += m2(i,j);
+
+	return m1;
+}
+
+
+AutoMarker::AutoMarker()
+{
+	// no AutoMarker can be trusted right away..
+	m_trusted = false;
+	m_CATransmat.makeIdentity();
+}
+
+AutoMarker::~AutoMarker()
+{
+}
+
+// change in here which value/method to use!!!
+osg::Matrixd AutoMarker::getCATransMat()
+{
+	return getCATransMatMean();
+}
+
+
+osg::Matrixd AutoMarker::getCATransMatMean()
+{
+	osg::Matrixd mean;
+	osg::Matrixd denominator;
+	osg::Matrixd* currentM;
+	double number = 0;
+	
+	if (!m_caTransMatList.empty()){
+	
+		mean.set(0,0,0,0,
+				0,0,0,0,
+				0,0,0,0,
+				0,0,0,0);
+		// sum up all matrices
+		for (int i = 0; i < m_caTransMatList.size(); i++){
+			currentM = m_caTransMatList[i];
+			mean += *currentM;
+		}
+		// normalize sum
+		number = m_caTransMatList.size();
+		denominator.set(1/number,0,0,0,
+						0,1/number,0,0,
+						0,0,1/number,0,
+						0,0,0,1/number);
+		
+		mean.preMult(denominator);
+		//std::cout << "Mean: " << mean << std::endl;
+	 }
+	return mean;
+}
+// try that later
+osg::Matrixd AutoMarker::getCATransMatMedian()
+{
+	return osg::Matrixd();
+}
+// try that later, maybe used to reject arriving matrices that exceed certain bounds in variance
+osg::Matrixd AutoMarker::getCATransMatVariance()
+{
+	return osg::Matrixd();
+}
+	
+
+int AutoMarker::getNumberOfCATransMatSamples()
+{
+	return m_caTransMatList.size();
+}
+
+bool AutoMarker::isSeen()
+{
+	return m_seen;
+}
+
+double AutoMarker::getConfidence()
+{
+	return m_confidence;
+}
+
+void AutoMarker::setConfidence(double cf)
+{
+	m_confidence = cf;
+}
+
+void AutoMarker::addCATransMat(osg::Matrixd* matrix)
+{
+	// if list exceeds max size, remove oldest entry and add newest matrix
+	if (m_caTransMatList.size() >= CATRANSLISTSIZE && !m_caTransMatList.empty() ){
+		m_caTransMatList.erase(m_caTransMatList.begin(),m_caTransMatList.begin()+1);
+		m_caTransMatList.push_back(matrix);
+	}
+	// else add newest matrix
+	else {
+		m_caTransMatList.push_back(matrix);
+	}
+}
+
+void AutoMarker::setValid()
+{
+	m_valid = true;
+}
+void AutoMarker::setSeen()
+{
+	m_seen = true;
+}
+
+
+void AutoMarker::overrideupdateTransform(const osg::Matrix& transform){
+	m_valid = true;
+	m_seen = true;
+	m_active = true;
+	updateTransform(transform);
+}
+
+
+bool AutoMarker::isTrusted()
+{
+	return m_trusted;
+}
+
+void AutoMarker::isTrusted(bool val)
+{
+	m_trusted = val;
+}
---
+++ src/osgART/Tracker/ARToolKitMMAuto/AutoMarker.h
@@ -0,0 +1,66 @@
+#ifndef AUTOMARKER_H
+#define AUTOMARKER_H	1
+
+#include <osg/Matrix>
+#include <osgUtil/SceneView>
+#include <osgUtil/IntersectVisitor>
+
+#include <osgART/ARTTransform>
+#include <osgART/SingleMarker>
+
+#include <string>
+#include <Map>
+#include <List>
+
+
+#define CATRANSLISTSIZE 50 // number of CandidateMarker->AnchorMarker transformation matrices kept in memory for averaging
+
+
+class AutoMarker : public osgART::SingleMarker{
+
+
+public:
+
+	AutoMarker();
+	~AutoMarker();
+
+	osg::Matrixd	getCATransMat();
+	osg::Matrixd	getCATransMatMean();
+	osg::Matrixd	getCATransMatMedian();
+	osg::Matrixd	getCATransMatVariance();
+    		
+	int				getNumberOfCATransMatSamples();
+	
+	bool			isSeen();
+	
+	double			getConfidence();
+	void			setConfidence(double cf);
+	void			addCATransMat(osg::Matrixd*);
+
+	void			setValid();
+	void			setSeen();
+	
+	void			overrideupdateTransform(const osg::Matrix& transform);
+
+
+
+	bool isTrusted();
+	void isTrusted(bool);
+
+protected:
+
+	// list of transformation matrices AnchorMarker -> thisCandidateMarker
+	typedef std::vector< osg::Matrixd* > CATransList;
+	CATransList m_caTransMatList;
+
+	osg::Matrixd	m_CATransmat;
+	osg::Matrixd	m_CATransmatMean;
+	osg::Matrixd	m_CATransmatMedian;
+	osg::Matrixd	m_CATransmatVariance;
+	osgART::Marker* m_Marker;
+	bool			m_trusted;
+
+private:
+
+};
+#endif
