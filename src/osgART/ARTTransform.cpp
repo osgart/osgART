5b2a1681316008809593413678edbe852cb1414d
hse25 <hse25@c80831f2-abe5-5e40-be4e-a8b7f943aa1a>
2007-03-26 23:49:39 +0000

    - ARTransformCallback is being moved into the ARTransform - The callback is a non-public interface
    git-svn-id: svn://hitlfs.canterbury.ac.nz/osgART_NG/trunk@527 c80831f2-abe5-5e40-be4e-a8b7f943aa1a

--- src/osgART/ARTTransform.cpp
+++ src/osgART/ARTTransform.cpp
@@ -21,6 +21,69 @@
 
 namespace osgART {
 
+	/**
+	 * \class ARTTransformCallback
+	 * \brief Node callback that applies a marker's transformation 
+	 * from ARToolKit to an OSG MatrixTransform.
+	 */
+	class ARTTransformCallback : public osg::NodeCallback {
+	public:
+
+		ARTTransformCallback(Marker* marker);
+	
+		virtual void operator()(osg::Node* node, osg::NodeVisitor* nv);
+
+	protected:
+	
+		Marker* m_marker;
+		
+	};
+
+
+	ARTTransformCallback::ARTTransformCallback(Marker* marker) : 
+		osg::NodeCallback(),
+		m_marker(marker)
+	{
+	}
+	
+	/*virtual*/ 
+	void ARTTransformCallback::operator()(osg::Node* node, osg::NodeVisitor* nv) {
+
+		// Make sure the visitor will return to invisible nodes
+		// If we make this node invisible (because the marker is hidden) then
+		// we need to be able to return and update it later, or it will remain
+		// hidden forever. 
+		nv->setNodeMaskOverride(0xFFFFFFFF);
+
+		osg::MatrixTransform* mt = dynamic_cast<osg::MatrixTransform*>(node);
+
+		if (mt) 
+		{
+
+			if (m_marker->isValid()) 
+			{
+				// copy matrix
+				mt->setMatrix(m_marker->getTransform());
+
+				// Visible
+				mt->setNodeMask(0xFFFFFFFF);
+			} 
+			else 
+			{
+				// Hidden
+				mt->setNodeMask(0x0);
+			}
+
+		}
+	
+		// must traverse the Node's subgraph            
+		traverse(node,nv);
+
+	}
+
+
+
+
 	ARTTransform::ARTTransform(Marker* marker) :  
 		osg::MatrixTransform(),
 		m_marker(marker)
--- src/osgART/ARTTransformCallback.cpp
+++ src/osgART/ARTTransformCallback.cpp
@@ -17,45 +17,6 @@
 namespace osgART {
 
 
-	ARTTransformCallback::ARTTransformCallback(Marker* marker) : 
-		osg::NodeCallback(),
-		m_marker(marker)
-	{
-	}
-	
-	/*virtual*/ 
-	void ARTTransformCallback::operator()(osg::Node* node, osg::NodeVisitor* nv) {
-
-		// Make sure the visitor will return to invisible nodes
-		// If we make this node invisible (because the marker is hidden) then
-		// we need to be able to return and update it later, or it will remain
-		// hidden forever. 
-		nv->setNodeMaskOverride(0xFFFFFFFF);
-
-		osg::MatrixTransform* mt = dynamic_cast<osg::MatrixTransform*>(node);
-
-		if (mt) 
-		{
-
-			if (m_marker->isValid()) 
-			{
-				// copy matrix
-				mt->setMatrix(m_marker->getTransform());
-
-				// Visible
-				mt->setNodeMask(0xFFFFFFFF);
-			} 
-			else 
-			{
-				// Hidden
-				mt->setNodeMask(0x0);
-			}
-
-		}
-	
-		// must traverse the Node's subgraph            
-		traverse(node,nv);
-
-	}
+	// Deprecated
 
 };
