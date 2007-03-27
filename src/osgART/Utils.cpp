a3a328c3c0edecb242624c9ea803d5ef846532f9
hse25 <hse25@c80831f2-abe5-5e40-be4e-a8b7f943aa1a>
2007-03-27 01:25:14 +0000

    - cleaned out duplicated code for utility functions (should use their OSG equivalent)
    git-svn-id: svn://hitlfs.canterbury.ac.nz/osgART_NG/trunk@533 c80831f2-abe5-5e40-be4e-a8b7f943aa1a

--- src/osgART/Utils.cpp
+++ src/osgART/Utils.cpp
@@ -13,29 +13,12 @@
 
 #include "osgART/Utils"
 
-namespace osgART
-{
+namespace osgART{
 
 	std::string trim(std::string& s,const std::string& drop/* = " "*/)
 	{
 		std::string r=s.erase(s.find_last_not_of(drop)+1);
 		return r.erase(0,r.find_first_not_of(drop));
-	}
-	
-	void PrintMatrix(std::string name, const osg::Matrix &Transform)
-	{
-		// Print it out
-		std::cout << "Matrix : " << name << std::endl;
-		for (int j = 0; j < 4; j++)
-		{
-			for (int i = 0; i < 4; i++) 
-			{
-				std::cout << std::setw(10) << std::setprecision(5) << Transform(i, j) << "  ";
-			}
-			std::cout  << std::endl;
-		}
-		osg::notify() << " length : " << Transform.getTrans().length() << std::endl;
-		osg::notify() << std::endl;
 	}
 
 	osg::Matrix GenerateMatrix(const osg::Vec3d & _trans, const osg::Vec3d & _rot)
@@ -52,20 +35,9 @@ namespace osgART
 
 	osg::Matrix DiffMatrix(const osg::Matrix & _base, const osg::Matrix & _m2)
 	{
-		//osg::Matrix tempmatt;
 		osg::Matrix Base;
 		Base.invert(_base);
 		return _m2 * Base;
-	}
-
-	void PrintVector3D	(std::string name, const osg::Vec3d &Vec3d)
-	{
-		// Print it out
-		osg::notify() << "Vector3d : " << name <<  "=> ";
-		for (int j = 0; j < 3; j++) 
-			osg::notify() << std::setw(10) << std::setprecision(5) << Vec3d._v[j] << "  ";
-			
-		osg::notify() << std::endl;
 	}
 
 };//namespace osgART
