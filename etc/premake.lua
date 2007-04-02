da40a458f83a188ae0b78316ec751c19195a0cdc
hse25 <hse25@c80831f2-abe5-5e40-be4e-a8b7f943aa1a>
2007-04-02 05:16:11 +0000

    - added more tracker
    git-svn-id: svn://hitlfs.canterbury.ac.nz/osgART_NG/trunk@584 c80831f2-abe5-5e40-be4e-a8b7f943aa1a

--- etc/premake.lua
+++ etc/premake.lua
@@ -21,11 +21,13 @@ end
 --
 package = newpackage()
 package.path = project.path
+package.target = "osgART"
 package.name = "osgART"
 package.language = "c++"
 package.kind = "dll"
 
 package.bindir = "../../bin"
+package.objdir = "obj/" .. package.target
 
 package.defines = {
 	"OSGART_LIBRARY"
@@ -51,7 +53,6 @@ package.excludes = {
 	"../../src/osgART/VideoBillboard.cpp",
 	"../../src/osgART/VideoForeground.cpp",
 	"../../src/osgART/VideoBackground.cpp",
-	"../../src/osgART/VideoPlane.cpp",
 	"../../src/osgART/ShadowRenderer.cpp",
 	"../../src/osgART/PlaneARShadowRenderer.cpp",
 	}
@@ -63,6 +64,56 @@ package.links = { "osg", "osgDB"}
 if (OS == "windows") then
 	table.insert(package.links, {"OpenGL32", "OpenThreadsWin32" })
 end
+
+--
+-- osgART Introspection Wrapper
+--
+
+package = newpackage()
+package.path = project.path
+package.name = "osgWrapper osgART"
+package.target = "osgwrapper_osgART"
+package.language = "c++"
+package.kind = "dll"
+package.bindir = "../../bin"
+package.objdir = "obj/" .. package.target
+
+if (OS == "windows") then
+	table.insert(package.defines,"WIN32")
+	table.insert(package.defines,"_WINDOWS")
+end
+
+package.includepaths = {
+	"../../include",
+	"$(OSG_ROOT)/include"
+}
+
+package.libpaths = { "$(OSG_ROOT)/lib" }
+
+package.links = {
+	"osg",
+	"osgART",
+	"osgIntrospection",
+	"osgDB",
+}
+
+if (OS == "windows") then
+	table.insert(package.links,"OpenThreadsWin32")
+	table.insert(package.links,"OpenGL32")
+end
+
+
+package.files = {
+  matchfiles("../../src/osgWrappers/osgART/*.cpp"),
+}
+
+package.excludes = {
+	"../../src/osgWrappers/osgART/Field.cpp",
+	"../../src/osgWrappers/osgART/ShadowRenderer.cpp",
+	"../../src/osgWrappers/osgART/PlaneARShadow.cpp",
+
+}
+
 
 
 --
@@ -95,6 +146,8 @@ package.links = {
 	"osgProducer"
 }
 
+
+
 package.files = {
   matchfiles("../../src/osgARTTest/*.cpp"),
 }
@@ -118,13 +171,57 @@ end
 
 
 --
+-- Plugin ARToolKit 4 Tracker
+--
+package = createTrackerPlugin("ARToolKit 4 Professional","artoolkit4")
+
+table.insert(package.includepaths,"$(ARTOOLKIT_4_ROOT)/include")
+table.insert(package.libpaths,"$(ARTOOLKIT_4_ROOT)/lib")
+
+package.files = {
+  matchfiles("../../src/osgART/Tracker/ARToolKit4/*")
+}
+
+if (OS == "windows") then
+	table.insert(package.links,"AR")
+	table.insert(package.links,"ARmulti")
+	table.insert(package.links,"ARgsub_lite")
+	table.insert(package.links,"OpenGL32")
+	table.insert(package.links,"GLU32")
+end
+
+
+--
+-- Plugin ARToolKit 4 NFT Tracker
+--
+package = createTrackerPlugin("ARToolKit 4 NFT","artoolkit4nft")
+
+table.insert(package.includepaths,"$(ARTOOLKIT_4_ROOT)/include")
+table.insert(package.includepaths,"$(ARTOOLKIT_NFT_ROOT)/include")
+table.insert(package.libpaths,"$(ARTOOLKIT_NFT_ROOT)/lib")
+
+package.files = {
+  matchfiles("../../src/osgART/Tracker/ARToolKit4NFT/*")
+}
+
+if (OS == "windows") then
+	table.insert(package.links,"AR")
+	table.insert(package.links,"AR2")
+	table.insert(package.links,"ARmulti")
+	table.insert(package.links,"ARgsub")
+	table.insert(package.links,"ARgsub_lite")
+	table.insert(package.links,"OpenGL32")
+	table.insert(package.links,"GLU32")
+end
+
+--
 -- Video Plugin: PointGrey
 --
 if (OS == "windows") then
 	package = createVideoPlugin("PointGrey","ptgrey")
 
-    table.insert(package.includepaths,"C://Program Files//Point Grey Research//PGR FlyCapture//include")
-	table.insert(package.libpaths,"C://Program Files//Point Grey Research//PGR FlyCapture//lib")
+    table.insert(package.includepaths,"$(ProgramFiles)/Point Grey Research/PGR FlyCapture/include")
+	table.insert(package.libpaths,"$(ProgramFiles)/Point Grey Research/PGR FlyCapture/lib")
 	table.insert(package.links,"PGRFlyCapture")
 	table.insert(package.links,"OpenThreadsWin32")
 
@@ -141,8 +238,8 @@ end
 if (OS == "windows") then
 	package = createVideoPlugin("Intranel RTSP","intranel")
 
-    table.insert(package.includepaths,"$(DXSDK_DIR)//Extras//DirectShow//Samples//C++//DirectShow//BaseClasses")
-	table.insert(package.libpaths,"$(DXSDK_DIR)//Extras//DirectShow//Samples//C++//DirectShow//BaseClasses//Release")
+    table.insert(package.includepaths,"$(DXSDK_DIR)/Extras/DirectShow/Samples/C++/DirectShow/BaseClasses")
+	table.insert(package.libpaths,"$(DXSDK_DIR)/Extras/DirectShow/Samples//C++/DirectShow/BaseClasses/Release")
 	table.insert(package.links,"strmbase")
 	table.insert(package.links,"strmiids")
 	table.insert(package.links,"winmm")
@@ -158,7 +255,6 @@ end
 --
 -- Plugin ARToolKit Video
 --
-
 package = createVideoPlugin("ARToolKit","artoolkit")
 
 if (OS == "windows") then
