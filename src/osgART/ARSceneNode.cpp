b85703920a8d34b7629a617e6466125697dc656a
prl25 <prl25@c80831f2-abe5-5e40-be4e-a8b7f943aa1a>
2007-04-11 10:00:25 +0000

    Add copyright headers. Still more to add to..
    git-svn-id: svn://hitlfs.canterbury.ac.nz/osgART_NG/trunk@610 c80831f2-abe5-5e40-be4e-a8b7f943aa1a

--- src/osgART/ARSceneNode.cpp
+++ src/osgART/ARSceneNode.cpp
@@ -1,7 +1,18 @@
+/*
+*	osgART/ARSceneNode
+ *	osgART: AR ToolKit for OpenSceneGraph
+ *
+ *	Copyright (c) 2005-2007 ARToolworks, Inc. All rights reserved.
+ *	
+ *	Rev		Date		Who		Changes
+ *  1.0   	2006-12-08  ---     Version 1.0 release.
+ *
+ */
+// @@OSGART_LICENSE_HEADER_BEGIN@@
+// @@OSGART_LICENSE_HEADER_END@@
+
 #include <osg/FrameStamp>
 #include <osg/Notify>
-
-
 #include "osgART/ARSceneNode"
 
 
--- src/osgART/VideoImageStream.cpp
+++ src/osgART/VideoImageStream.cpp
@@ -1,3 +1,16 @@
+/*
+*	osgART/VideoImageStream
+ *	osgART: AR ToolKit for OpenSceneGraph
+ *
+ *	Copyright (c) 2005-2007 ARToolworks, Inc. All rights reserved.
+ *	
+ *	Rev		Date		Who		Changes
+ *  1.0   	2006-12-08  ---     Version 1.0 release.
+ *
+ */
+// @@OSGART_LICENSE_HEADER_BEGIN@@
+// @@OSGART_LICENSE_HEADER_END@@
+
 #include "osgART/VideoImageStream"
 
 /**
