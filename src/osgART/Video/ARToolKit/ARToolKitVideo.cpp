cc3c63353932aead8d19a0e14ede745070493ec9
jcl64 <jcl64@c80831f2-abe5-5e40-be4e-a8b7f943aa1a>
2010-08-02 22:42:34 +0000

    git-svn-id: svn://hitlfs.canterbury.ac.nz/osgART_NG/trunk@906 c80831f2-abe5-5e40-be4e-a8b7f943aa1a

--- src/osgART/Video/ARToolKit/ARToolKitVideo.cpp
+++ src/osgART/Video/ARToolKit/ARToolKitVideo.cpp
@@ -220,9 +220,19 @@ ARToolKitVideo::open()
 		// get the video size
 		ar2VideoInqSize(video, &xsize, &ysize);
 
+		float fps = 0.0f;
+
+#ifdef WIN32		
+		ar2VideoInqFreq(video, &fps);
+#endif
+
 		// report the actual
 		osg::notify() << std::dec << "ARToolKitVideo::open() size of video " <<
-			xsize << " x " << ysize << std::endl;
+			xsize << " x " << ysize << ", fps: " << fps << std::endl;
+
+		m_config.selectedWidth = xsize;
+		m_config.selectedHeight = ysize;
+		m_config.selectedFrameRate = fps;
 
 	}
 
